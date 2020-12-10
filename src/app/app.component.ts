a2735783972c8fe34a42ba698ea03998afc2aff7
quyetdc <quyetdc.uet@gmail.com>
2020-12-10 14:41:23 +0700

    Basically we can get Component data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+  passwordLength = 0;
+
+  onChangeUseLetters() {
+    this.useLetters = !this.useLetters;
+    this.printValues();
+  }
+
+  onChangeUseNumbers() {
+    this.useNumbers = !this.useNumbers;
+    this.printValues();
+  }
+
+  onChangeUseSymbols() {
+    this.useSymbols = !this.useSymbols;
+    this.printValues();
+  }
+
+  // We need to tell TypeScripe this Event target is HTMLInputElement
+  // So we can avoid bug undefine value on TypeScript EventTarget
+  onChangeLength(event: Event) {
+    if (event.target) {
+      const parsedValue = parseInt((event.target as HTMLInputElement).value);
+      if (!isNaN(parsedValue)) {
+        this.passwordLength = parsedValue;  
+      }
+    }
+    
+    this.printValues();
+  }
+
+  printValues() {
+    console.log('use_letters: ', this.useLetters);
+    console.log('use_numbers: ', this.useNumbers);
+    console.log('use_symbols: ', this.useSymbols);
+    console.log('password_length: ', this.passwordLength);
+    console.log('password: ', this.password);
+  }
 
   onButtonClick() {
     console.log("Button Clicked!");
